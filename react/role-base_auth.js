0332ba7c6964a9180310d0aeae8fb1d211a5a315
fodela <particleme30@gmail.com>
2022-11-15 07:19:25 +0000

    react: role-based authentication

---
+++ react/role-base_auth.js
@@ -0,0 +1,40 @@
+// React Protected Routes | Role-Based Authorization
+import React from "react";
+
+/*
+Using custom hooks -- best practice
+*/
+// import or create AuthContext
+// import AuthContext from "../context/AuthProvider"
+const AuthContext = React.createContext();
+
+const useAuth = () => {
+  return React.useContext(AuthContext);
+};
+
+export default useAuth;
+
+// Where we want to use the useAuth custom hooks
+// import useAuth from '../hooks/useAuth'
+const { setAuth1 } = useAuth();
+
+/*
+Without custom hooks
+*/
+import { useContext } from "react";
+// import AuthContext from '../context/AuthContext'
+const { setAuth2 } = useContext(AuthContext);
+
+// Create requireAuth component
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+
+const RequireAuth = () => {
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  return auth?.user ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+};
