e4de72f25ceaf7ba69f0dd8140555bbc2a018114
fodela <particleme30@gmail.com>
2022-11-19 06:39:05 +0000

    js| generators reinstantiating generators

--- js/generators.js
+++ js/generators.js
@@ -9,3 +9,7 @@ console.log(seq.next())  // {value:undefined, done:true}
 
 
 
+// gen() will be treated as new instance every single time.
+console.log(gen().next()) //{value:10, done:false}
+console.log(gen().next()) //{value:10, done:false}
+console.log(gen().next()) //{value:10, done:false}
