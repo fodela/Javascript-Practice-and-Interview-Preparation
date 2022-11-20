8b384ae43bcf2328708da46db0bae8caf6850828
fodela <particleme30@gmail.com>
2022-11-20 06:44:16 +0000

    js| generators reinstantiating generators

--- js/generators.js
+++ js/generators.js
@@ -13,3 +13,18 @@ console.log(seq.next())  // {value:undefined, done:true}
 console.log(gen().next()) //{value:10, done:false}
 console.log(gen().next()) //{value:10, done:false}
 console.log(gen().next()) //{value:10, done:false}
+
+function* gen1(){
+    console.log("first")
+    yield 10
+    console.log("second")
+    yield 20
+    console.log("third")
+    yield 30
+    console.log("fourth")
+}
+
+console.log(seq.next())  // first {value:10, done:false}
+console.log(seq.next())  // second {value:20, done:false}
+console.log(seq.next())  // third {value:30, done:false}
+console.log(seq.next())  // fourth {value:undefined, done:true}
