d55680079f4d3c1537a3fe9618e05398c1b2836e
fodela <particleme30@gmail.com>
2023-02-10 19:03:25 +0000

    feat: express redirect

--- node/intro/server.js
+++ node/intro/server.js
@@ -10,5 +10,12 @@ app.get("^/$|/index(.html)?", (req, res) => {
 
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
