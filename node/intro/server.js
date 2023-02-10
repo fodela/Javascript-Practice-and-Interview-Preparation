8fcda0263aef06e91786bdae684a58ad88f97aa4
fodela <particleme30@gmail.com>
2023-02-10 19:02:28 +0000

    feat: express sendFile

--- node/intro/server.js
+++ node/intro/server.js
@@ -5,4 +5,10 @@ const path = require("path");
 
 const app = express();
 
+app.get("^/$|/index(.html)?", (req, res) => {
+  //   res.sendFile("./views/index.html", { root: __dirname });
+
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
