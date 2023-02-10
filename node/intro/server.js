e364437558342848de7f977465dfa27090d6ac3a
fodela <particleme30@gmail.com>
2023-02-10 19:41:21 +0000

    feat: route handlers and function chaining

--- node/intro/server.js
+++ node/intro/server.js
@@ -1,5 +1,4 @@
-// const PORT = process.env.PORT || 3500;
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
 const express = require("express");
 const path = require("path");
 
@@ -18,8 +17,35 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html");
 });
 
+// Route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("Loading hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello Fodela!!!");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("FuncOne");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("FuncTwo");
+  next();
+};
+const three = (req, res) => {
+  console.log("FuncThree");
+  res.send("Finished!");
+};
+// Function chaining
+app.get("/chain(.html)?", [one, two, three]);
+
+// Default route -> 404
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
