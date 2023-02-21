51c290c141d875261da8082be72e0d8e62c64536
fodela <particleme30@gmail.com>
2023-02-21 08:07:08 +0000

    feat: express routes

--- node/intro/server.js
+++ node/intro/server.js
@@ -18,6 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // handle static files
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/subdir", express.static(path.join(__dirname, "public")));
+
+// Provide routes
+app.use("/subdir", require("./routes/subdir"));
+
+app.use("/", require("./routes/root"));
+
+app.use("/employees", require("./routes/api/employees"));
 
 // Third party
 // Cross Origin Resource Sharing
@@ -38,58 +46,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// Routes
-app.get("^/$|/index(.html)?", (req, res) => {
-  //   res.sendFile("./views/index.html", { root: __dirname });
-
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html");
-});
-
-// Route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("Loading hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello Fodela!!!");
-  }
-);
-
-const one = (req, res, next) => {
-  console.log("FuncOne");
-  next();
-};
-const two = (req, res, next) => {
-  console.log("FuncTwo");
-  next();
-};
-const three = (req, res) => {
-  console.log("FuncThree");
-  res.send("Finished!");
-};
-// Function chaining
-app.get("/chain(.html)?", [one, two, three]);
-
-// Default route -> 404
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json("404 Not Found");
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
-});
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
